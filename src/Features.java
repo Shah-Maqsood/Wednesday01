cd2726dd58492ad8c8ba71f68032b96631583356
Shah-Maqsood <koelnbonn99@gmail.com>
2022-12-21 01:10:41 +0100

    First class comments

---
+++ src/Features.java
@@ -0,0 +1,7 @@
+public class Features {
+    int a;
+    int b;
+    String c;
+    char d;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Features xx=new Features();
+        xx.a=11;
+        xx.b=10;
+
+
+
+        System.out.println("Hello world!"+xx.a+xx.b+xx.c+xx.d);
+    }
+}
